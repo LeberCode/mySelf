defd3bb6f9602d6adb9ed1d986ea832b5681673c
Robin Leber <51661454+LeberKot@users.noreply.github.com>
2022-06-18 18:31:37 +0200

    changed header opacity
    test for build with aws S3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
     return (
         <div className="w-screen h-screen bg-heroForHeader bg-cover bg-center bg-fixed z-0 bg-no-repeat">
         {/* Foto von Erik Mclean von Pexels: https://www.pexels.com/de-de/foto/spielzeug-superheld-nahe-laptop-auf-tisch-zu-hause-4048093/ */}
-            <div className="flex flex-col justify-center items-center m-auto w-screen h-screen bg-signal opacity-70 divide-y-2">
+            <div className="flex flex-col justify-center items-center m-auto w-screen h-screen bg-signal bg-opacity-50 divide-y-2">
                 <Text className="flex">
                     Robin Leber  
                 </Text>
