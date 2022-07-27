1a9c33982e70a5152e9d3ff47d3e6773ce3a1acf
Robin Leber <51661454+LeberKot@users.noreply.github.com>
2022-07-27 17:43:27 +0200

    interests more flexible

--- src/components/OwnInterest.tsx
+++ src/components/OwnInterest.tsx
@@ -4,6 +4,7 @@ import SectionHeader from "./SectionHeader";
 import meSport from "../assets/images/meSport.jpeg";
 import meHexe from "../assets/images/27-Leber-schmuck.jpg";
 import meReisen3 from "../assets/images/Reisen3.jpeg";
+import ReisenTrolltunga from "../assets/images/ReisenTrolltunga.jpeg"
 import { Link } from '@material-ui/icons';
 import data from '../assets/data/interestData.json';
 import { Interest } from "../types/Interest";
@@ -32,11 +33,11 @@ const OwnInterest = (props: ChildrenProps) => {
                     interests.map( (interest:Interest, index ) => {
                         return(
                             <div className={`
-                                lg:flex lg:h-80 h-fit bg-white m-4 mt-8 max-w-fit min-w-fit text-left relative rounded-lg mx-auto lg:mx-2
+                                lg:flex h-fit bg-white m-4 mt-8 max-w-fit min-w-fit text-left relative rounded-lg mx-auto lg:mx-2
                                 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:shadow-lg
                                 ${index % 2 === 0 ? "lg:float-left lg:rounded-l-lg" : "lg:float-right lg:rounded-r-lg 4k:float-left"}
                             `}>
-                                <img src={mapPictures.get(index+1)} alt="Volleyball"
+                                <img src={mapPictures.get(index+1)} alt="Image of interest."
                                     className={
                                         `w-96 object-cover order-first rounded-t-lg
                                         ${index % 2 === 0 ? "lg:order-first lg:rounded-none lg:rounded-l-lg" : "lg:order-last lg:rounded-none lg:rounded-r-lg 4k:order-first 4k:rounded-none 4k:rounded-l-lg"}`
@@ -46,7 +47,7 @@ const OwnInterest = (props: ChildrenProps) => {
                                     <p className="font-semibold text-xl">
                                         {interest.title}
                                     </p>
-                                    <div className="pt-2 text-black mb-8 lg:mb-0">
+                                    <div className="pt-2 text-black mb-12">
                                         <ul className="list-disc pl-4">
                                             {interest.description.map(descriptionPart => {
                                                 return (
@@ -65,7 +66,7 @@ const OwnInterest = (props: ChildrenProps) => {
                                                 {interest.link}
                                             </a>
                                         </div>
-                                        : null
+                                        : <div className="-mt-12"/>
                                     }
                                 </div>
                             </div>
